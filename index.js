0eb43e5a0ff5ba4a58a8f34e31a677d4a4b60474
Joao Luna <160221006@estudantes.ips.pt>
2019-05-02 17:41:04 +0100

    Move R-IoT to a whole new repository

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-module.exports.BITalinoRIOT = require('./bitalino-riot');
 module.exports.BITalino = require('./bitalino').BITalino;
 module.exports.ErrorCode = require('./bitalino').ErrorCode;
 module.exports.createBITalino = require('./bitalino').createBITalino;
